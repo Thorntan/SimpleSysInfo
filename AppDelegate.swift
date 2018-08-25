884cade391250a0bffd8b9c08ee727ccbe7adc41
yanlihua <yanlh@jpush.cn>
2018-08-25 23:38:27 +0800

    add a script

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleSysInfo
+//
+//  Created by viva on 24/8/2018.
+//  Copyright © 2018 viva. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
